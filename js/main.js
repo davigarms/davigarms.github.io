ca9948ee43d0ecfff2d989c3007acbf390a7663a
Davi Garms <davigarms@gmail.com>
2020-12-26 15:56:40 +0000

    Tweak flex box properties and remove scrollTo in mobile

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ const initEvents = () => {
 }
 
 const handleClick = (e) => {
-    e.preventDefault();
-    const targetId = document.querySelector(e.target.getAttribute("href"));
-    if (window.history && window.history.pushState) history.pushState("", document.title, e.target.getAttribute("href"));
-    gsap.to(window, {duration: 1.5, ease: "power2.inOut", scrollTo:targetId});
+    if (window.innerWidth > 992) {
+        e.preventDefault();
+        const targetId = document.querySelector(e.target.getAttribute("href"));
+        if (window.history && window.history.pushState) history.pushState("", document.title, e.target.getAttribute("href"));
+        gsap.to(window, {duration: .75, ease: "power2.inOut", scrollTo: targetId});
+    }
 }
 
 initEvents();
