c97ebe621d45edca1b12a55384bb248e91007b72
Davi Garms <davigarms@gmail.com>
2020-12-26 17:01:46 +0000

    Implement methods to set active section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+const sections = Array.from(document.querySelectorAll("section"));
+
 const initEvents = () => {
     const navButtons = Array.from(document.querySelectorAll("button a")).filter(button => button.getAttribute("button-nav") !== null);
     navButtons.forEach(button =>  button.addEventListener("click", handleClick));
+    window.addEventListener("scroll", handleScroll);
+    activateSection(getSection());
 }
 
 const handleClick = (e) => {
@@ -10,6 +14,27 @@ const handleClick = (e) => {
         if (window.history && window.history.pushState) history.pushState("", document.title, e.target.getAttribute("href"));
         gsap.to(window, {duration: .75, ease: "power2.inOut", scrollTo: targetId});
     }
+}
+
+const handleScroll = (e) => {
+    activateSection(getSection());
+}
+
+const getSection = () => {
+    let activeIndex = 0;
+    sections.forEach((section, index) => {
+        const next = sections[index+1] === undefined ? sections[index] : sections[index+1];
+        if (window.pageXOffset >= section.offsetLeft && 
+            window.pageXOffset <= next.offsetLeft) {
+            activeIndex = index;
+        }
+    });
+    return sections[activeIndex];
+}
+
+const activateSection = (activeSection) => {
+    sections.forEach(section => section.classList.remove('active'));
+    activeSection.classList.add('active');
 }
 
 initEvents();
