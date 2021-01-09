1fb748b3b8f49ef2595ca62d768c6ccc7ae5c9db
Davi Garms <davigarms@gmail.com>
2021-01-09 20:04:19 +0000

    Implement email form

--- js/main.js
+++ js/main.js
@@ -1,100 +1,154 @@
-const wrapper = document.querySelector(".wrapper");
-const sections = Array.from(document.querySelectorAll(".section"));
-const themeButton = document.querySelector("li.themes-button button");
-const navButtons = Array.from(document.querySelectorAll("button a")).filter(button => button.getAttribute("nav-button") !== null);
-const menuItems = document.querySelectorAll(".side-menu ul li");
-const sideMenu = document.querySelector(".side-menu");
-const menuButton = document.querySelector(".side-menu-button");
-const overlay = document.querySelector(".side-menu .overlay");
-const documentTitle = document.title.substr(document.title.indexOf(">")+1);
-let isNewState = true;
-let activeIndex;
+window.addEventListener('load', function(){
+    const wrapper = document.querySelector('.wrapper');
+    const sections = [...document.querySelectorAll('.section')];
+    const themeButton = document.querySelector('li.themes-button button');
+    const navButtons = [...document.querySelectorAll('button a')].filter(i => i.getAttribute('nav-button') !== null);
+    const menuItems = document.querySelectorAll('.side-menu ul li');
+    const sideMenu = document.querySelector('.side-menu');
+    const menuButton = document.querySelector('.side-menu-button');
+    const overlay = document.querySelector('.side-menu .overlay');
+    const documentTitle = document.title.substr(document.title.indexOf('>')+1);
+    const form = document.getElementById('form');
+    const submitButton = document.getElementById('submit');
+    const formStatus = document.getElementById('formStatus');
+    const formFields = [...form.elements].filter(i => i.required);
+    let isNewState = true;
+    let activeIndex;
 
-const init = (e) => {
-    initEvents();
-    const initialPage = window.location.hash === '' ?  '#home' : window.location.hash;
-    setTimeout(() => switchSection(sections.indexOf(document.querySelector(initialPage))), 50);
-}
+    const init = () => {
+        initEvents();
+        const initialPage = window.location.hash === '' ?  '#home' : window.location.hash;
+        setTimeout(() => switchSection(sections.indexOf(document.querySelector(initialPage))), 50);
+    }
 
-const initEvents = () => {
-    navButtons.forEach(button =>  button.addEventListener("click", handleNavClick));
-    menuItems.forEach(button =>  button.addEventListener("click", handleMenuItemClick));
-    themeButton.addEventListener("click", handleThemeClick);
-    menuButton.addEventListener("click", handleMenuClick);
-    overlay.addEventListener("click", handleMenuClick);
-    wrapper.addEventListener("scroll", handleScroll);
-    window.addEventListener("popstate", handlePopState);
-}
+    const initEvents = () => {
+        wrapper.addEventListener('scroll', handleScroll);
+        navButtons.forEach(i =>  i.addEventListener('click', handleNavClick));
+        menuItems.forEach(i =>  i.addEventListener('click', handleMenuItemClick));
+        formFields.forEach(i => i.addEventListener('focus', handleFocus));
+        submitButton.addEventListener('click', handleSubmit);
+        themeButton.addEventListener('click', handleThemeClick);
+        menuButton.addEventListener('click', handleMenuClick);
+        overlay.addEventListener('click', handleMenuClick);
+        window.addEventListener('popstate', handlePopState);
+    }
 
-const handleMenuClick = (e) => {
-    sideMenu.classList.contains("active") ? sideMenu.classList.remove("active") :  sideMenu.classList.add("active");
-}
+    const success = () => {
+        form.reset();
+        submitButton.style = 'display: none';
+        formStatus.innerHTML = 'Thanks!';
+    }
 
-const handleMenuItemClick = (e) => {
-    e.preventDefault();
-    gotoSection(document.querySelector(e.target.getAttribute("href")));
-    handleMenuClick();
-}
+    const error = () => {
+        formStatus.innerHTML = 'Oops! Please try again later.';
+    }
 
-const handleThemeClick = (e) => {
-    document.body.classList.contains("dark") ? document.body.classList.remove("dark") :  document.body.classList.add("dark");
-}
+    const handleFocus = (e) => {
+        e.target.parentElement.classList.remove('error');
+    }
 
-const handleNavClick = (e) => {
-    e.preventDefault();
-    gotoSection(document.querySelector(e.target.getAttribute("href")));
-}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(form);
+        if (isValidated()) sendMail(form.method, form.action, data, success, error);
+    }
 
-const handlePopState = (e) => {
-    isNewState = false;
-    setTimeout(() => gotoSection(document.querySelector("#"+history.state.section)), 50);
-}
+    const isValidated = () => {
+        let validated = true;
+        formFields.forEach((i) => {
+            if (i.value === '') {
+                i.parentElement.classList.add('error');
+                validated = false;
+            }
+        })
+        return validated;
+    }
+    
+    const sendMail = (method, url, data, success, error) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                success(xhr.response, xhr.responseType);
+            } else {
+                error(xhr.status, xhr.response, xhr.responseType);
+            }
+        };
+        xhr.send(data);
+    }
 
-const gotoSection = (section) => {
-    section.scrollIntoView();
-}
+    const handleMenuClick = (e) => {
+        sideMenu.classList.contains('active') ? sideMenu.classList.remove('active') :  sideMenu.classList.add('active');
+    }
 
-const handleScroll = (e) => {
-    switchSection(getSectionIndex());
-}
+    const handleMenuItemClick = (e) => {
+        e.preventDefault();
+        gotoSection(document.querySelector(e.target.getAttribute('href')));
+        handleMenuClick();
+    }
 
-const getSectionIndex = () => {
-    let activeIndex = 0;
-    sections.forEach((section, index) => {
-        if (wrapper.scrollLeft >= section.offsetWidth * index - window.innerWidth/2 && 
-            wrapper.scrollLeft < section.offsetWidth * (index + 1)) {
-            activeIndex = index;
-        }
-    });
-    return activeIndex;
-}
+    const handleThemeClick = (e) => {
+        document.body.classList.contains('dark') ? document.body.classList.remove('dark') :  document.body.classList.add('dark');
+    }
 
-const switchSection = (index) => {
-    if (activeIndex===index) return;
-    activeIndex = index;
-    sections.forEach((section, i) => {
-        menuItems[i].classList.remove('active');
-        section.classList.remove('active');
-        section.classList.remove('reverted');
-        if (i < index) section.classList.add('reverted');
-    });
+    const handleNavClick = (e) => {
+        e.preventDefault();
+        gotoSection(document.querySelector(e.target.getAttribute('href')));
+    }
 
-    sections[index].classList.add('active')
-    menuItems[index].classList.add('active');
-    setTopContent(index);
-    setState(sections[index]);
-}
+    const handlePopState = (e) => {
+        isNewState = false;
+        setTimeout(() => gotoSection(document.querySelector('#'+history.state.section)), 50);
+    }
 
-const setTopContent = (index) => {
-    if (sections[index].querySelector(".content")) sections[index].querySelector(".content").scrollTop = 0;
-}
+    const gotoSection = (section) => {
+        section.scrollIntoView();
+    }
 
-const setState = (section) => {
-    const title = `Davi Garms > ${section.id.substr(0,1).toUpperCase()+section.id.substr(1)} | ${documentTitle}`;
-    if (history.state && document.title !== history.state.title) document.title = title;
-    if (isNewState && window.history && window.history.pushState) history.pushState({section: section.id, title }, title, "#" + section.id);
-    isNewState = true;
-}   
+    const handleScroll = (e) => {
+        switchSection(getSectionIndex());
+    }
 
-window.addEventListener("load", init);
+    const getSectionIndex = () => {
+        let activeIndex = 0;
+        sections.forEach((section, index) => {
+            if (wrapper.scrollLeft >= section.offsetWidth * index - window.innerWidth/2 && 
+                wrapper.scrollLeft < section.offsetWidth * (index + 1)) {
+                activeIndex = index;
+            }
+        });
+        return activeIndex;
+    }
+
+    const switchSection = (index) => {
+        if (activeIndex===index) return;
+        activeIndex = index;
+        sections.forEach((section, i) => {
+            menuItems[i].classList.remove('active');
+            section.classList.remove('active');
+            section.classList.remove('reverted');
+            if (i < index) section.classList.add('reverted');
+        });
+
+        sections[index].classList.add('active')
+        menuItems[index].classList.add('active');
+        setTopContent(index);
+        setState(sections[index]);
+    }
+
+    const setTopContent = (index) => {
+        if (sections[index].querySelector('.content')) sections[index].querySelector('.content').scrollTop = 0;
+    }
+
+    const setState = (section) => {
+        const title = `Davi Garms > ${section.id.substr(0,1).toUpperCase()+section.id.substr(1)} | ${documentTitle}`;
+        if (history.state && document.title !== history.state.title) document.title = title;
+        if (isNewState && window.history && window.history.pushState) history.pushState({section: section.id, title }, title, '#' + section.id);
+        isNewState = true;
+    }   
+
+    init();
+});
 
